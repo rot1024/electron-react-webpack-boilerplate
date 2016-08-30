6d104cec81c28f66f47a46c5920c831c24f7024e
rot1024 <rot1024@gmail.com>
2016-08-30 22:00:11 +0900

    Fix stylist config

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,6 +3,7 @@
 module.exports = {
   extends: "stylelint-config-standard",
   rules: {
+    "no-empty-source": null,
     "property-no-unknown": [true, { ignoreProperties: ["composes"] }],
     "selector-list-comma-newline-after": null
   }
