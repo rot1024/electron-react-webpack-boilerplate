86f9aa2c444535e19b5476fc0662f47947b0d82c
rot1024 <rot1024@gmail.com>
2016-09-01 17:31:23 +0900

    Add rebuild script

---
+++ tasks/rebuild.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const childProcess = require("child_process");
+/* eslint-disable node/no-unpublished-require */
+const electron = require("electron");
+const rebuild = require("electron-rebuild");
+/* eslint-enable node/no-unpublished-require */
+
+rebuild.shouldRebuildNativeModules(electron)
+  .then(shouldBuild => {
+    if (!shouldBuild) {
+      console.log("It is not neccessary to rebuild native modules.");
+      return true;
+    }
+
+    const electronVersion = childProcess.execSync(`${electron} --version`, {
+      encoding: "utf8",
+    }).match(/v(\d+\.\d+\.\d+)/)[1];
+
+    console.log(`Rebuilding native modules for electron v${electronVersion} ...`);
+
+    return rebuild.installNodeHeaders(electronVersion)
+      .then(() => rebuild.rebuildNativeModules(electronVersion, "./node_modules"));
+  })
+  .then(() => {
+    console.log("done!");
+  })
+  .catch(e => {
+    console.error("Building modules didn't work!");
+    console.error(e.stack || e);
+  });
