793a2ac969d9007c9dbaf58574cdb2d61d52e026
rot1024 <rot1024@gmail.com>
2017-04-01 16:55:04 +0900

    enable electron-packager overwrite

--- tasks/package.js
+++ tasks/package.js
@@ -3,7 +3,6 @@
 const os = require("os");
 const packager = require("electron-packager");
 const argv = require("minimist")(process.argv.slice(2));
-const del = require("del");
 const pkg = require("../package.json");
 
 const shouldBuildAll = argv.all || false;
@@ -18,30 +17,25 @@ const options = {
     /^\/(?!build|package.json$)/
   ],
   name: pkg.productName || pkg.name,
-  out: "dist"
+  out: "dist",
+  overwrite: true
 };
 
-del("dist")
-  .then(() => {
-    const archs = shouldBuildAll ? ["ia32", "x64"] : [os.arch()];
-    const platforms = shouldBuildAll ? ["linux", "win32", "darwin"] : [os.platform()];
-    platforms.forEach(platform => {
-      archs.forEach(arch => {
-        console.log(`${platform} ${arch} start`);
-        pack(platform, arch).then(
-          () => console.log(`${platform} ${arch} finish`),
-          err => {
-            if (!err) return;
-            console.log(`${platform} ${arch} error`);
-            throw err;
-          }
-        );
-      });
-    });
-  })
-  .catch(err => {
-    console.error(err);
+const archs = shouldBuildAll ? ["ia32", "x64"] : [os.arch()];
+const platforms = shouldBuildAll ? ["linux", "win32", "darwin"] : [os.platform()];
+platforms.forEach(platform => {
+  archs.forEach(arch => {
+    console.log(`${platform} ${arch} start`);
+    pack(platform, arch).then(
+      () => console.log(`${platform} ${arch} finish`),
+      err => {
+        if (!err) return;
+        console.log(`${platform} ${arch} error`);
+        throw err;
+      }
+    );
   });
+});
 
 function pack(platform, arch) {
   if (platform === "darwin" && arch === "ia32") {
